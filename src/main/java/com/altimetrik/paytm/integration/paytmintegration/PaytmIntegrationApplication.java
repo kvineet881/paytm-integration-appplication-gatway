c1d0217e06a0c9f67684792e98d5b69e517d4f66
Eureka server <kvineet881@gmail.com>
2020-11-27 18:15:42 +0530

    Paytm integration Application

---
+++ src/main/java/com/altimetrik/paytm/integration/paytmintegration/PaytmIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.altimetrik.paytm.integration.paytmintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaytmIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaytmIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/altimetrik/paytm/integration/paytmintegration/PaytmIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.altimetrik.paytm.integration.paytmintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaytmIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
